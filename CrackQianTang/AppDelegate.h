5be504df73972ac1a78dee8ea666e29e702fdb6d
MaAiNong <mylovercode@gmail>
2017-09-26 11:40:24 +0800

    Initial Commit

---
+++ CrackQianTang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrackQianTang
+//
+//  Created by X-MAN on 2017/9/26.
+//  Copyright © 2017年 maainong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrackQianTang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrackQianTang
+//
+//  Created by X-MAN on 2017/9/26.
+//  Copyright © 2017年 maainong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
